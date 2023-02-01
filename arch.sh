b02e9b56e8658c3a92c95c734f749f16ad10f1ab
karatun75 <karatun75@gmail.com>
2023-02-01 22:39:42 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # 1
-
-ping -c17 8.8.8.8
+if
+echo "ping -c17 8.8.8.8"
 
 # ls /dev/nvme*
 # ls /dev/sd*
 
-lsblk
+echo "lsblk"
 
-blkid
+echo "blkid"
 
-gdisk /dev/nvme0n1
+echo "gdisk /dev/nvme0n1"
 # mklabel gpt 
 # mkpart ESP fat32 1MiB 512MiB 
 # set 1 boot on 
@@ -19,23 +19,23 @@ gdisk /dev/nvme0n1
 # n
 
 
-cryptsetup luksFormat /dev/nvme0n1p4
+echo "cryptsetup luksFormat /dev/nvme0n1p4"
 
-cryptsetup open /dev/nvme0n1p4 luks
+echo "cryptsetup open /dev/nvme0n1p4 luks"
 
 # пароль  "luks"
 
-ls /dev/mapper/*
+echo "ls /dev/mapper/*"
 
 # сделать группу "Volume group". И в группе создать логические разделы "Logical volume":
 
-pvcreate /dev/mapper/luks
-vgcreate steel /dev/mapper/luks
-lvcreate -L 20G steel -n swap
-lvcreate -L 120G steel -n root
-lvcreate -l 100%FREE steel -n home
+echo "pvcreate /dev/mapper/luks"
+echo "vgcreate steel /dev/mapper/luks"
+echo "lvcreate -L 20G steel -n swap"
+echo "lvcreate -L 120G steel -n root"
+echo "lvcreate -l 100%FREE steel -n home"
 
-lvs
+echo "lvs"
 
 # $ lvs  LV   VG    Attr       LSize   Pool Origin Data%  Meta%  Move Log Cpy%Sync Convert  
 # root steel      -wi-a----- 120g  
@@ -44,39 +44,41 @@ lvs
 
 # Форматируем разделы
 
-mkfs.ext4 /dev/mapper/steel-root
-mkfs.ext4 /dev/mapper/steel-home
-mkswap /dev/mapper/steel-swap
+echo "mkfs.ext4 /dev/mapper/steel-root"
+echo "mkfs.ext4 /dev/mapper/steel-home"
+echo "mkswap /dev/mapper/steel-swap"
 
 # $ mkfs.fat -F32 /dev/nvme0n1p1
 
 # Монтируем
 
-mount /dev/mapper/steel-root /mnt
-mkdir -p /mnt/{home,boot/efi}
-mount /dev/nvme0n1p1 /mnt/boot/efi
-mount /dev/mapper/steel-home /mnt/home
-swapon /dev/mapper/steel-swap
+echo "mount /dev/mapper/steel-root /mnt"
+echo "mkdir -p /mnt/{home,boot/efi}"
+echo "mount /dev/nvme0n1p1 /mnt/boot/efi"
+echo "mount /dev/mapper/steel-home /mnt/home"
+echo "swapon /dev/mapper/steel-swap"
 
 
 # Зеркала устанавливаем
-
+echo "Server=http://mirrors.prok.pw/archlinux/$repo/os/$arch >> /etc/pacman.d/mirrorlist"
 nano /etc/pacman.d/mirrorlist
 
 # Server=http://mirrors.prok.pw/archlinux/$repo/os/$arch
 
 # Устанавливаем
 
-pacstrap -i /mnt base base-devel linux linux-firmware lvm2 vim dhcpcd net-tools iproute2 networkmanager os-prober mtools fuse tmux links wget git
+echo "pacstrap -i /mnt base base-devel linux linux-firmware lvm2 vim dhcpcd net-tools iproute2 networkmanager os-prober mtools fuse tmux links wget git"
  
 
 # Генерируем fstab
 
-genfstab -U /mnt >> /mnt/etc/fstab
+echo "genfstab -U /mnt >> /mnt/etc/fstab"
 
-cat /mnt/etc/fstab
+echo "cat /mnt/etc/fstab"
 
-arch-chroot /mnt
+echo "arch-chroot /mnt"
+
+fi
 
 
 
