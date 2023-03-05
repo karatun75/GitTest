4c73e153ec021a6387e4e36822eccdd8c2a4af5e
karatun75 <karatun75@gmail.com>
2023-03-05 23:41:46 +0300

    Update Gentoo1.sh

--- Gentoo1.sh
+++ Gentoo1.sh
@@ -104,11 +104,11 @@ mount --types proc /proc /mnt/gentoo/proc
 
 mount --rbind /sys /mnt/gentoo/sys
 
-# mount --make-rslave /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
 
 mount --rbind /dev /mnt/gentoo/dev
 
-# mount --make-rslave /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
 
 # CHROOT
 
