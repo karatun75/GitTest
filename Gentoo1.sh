19e52132c9edd328e9916c9f5ac75a5e4e72baa3
karatun75 <karatun75@gmail.com>
2023-03-06 20:37:59 +0300

    Update Gentoo1.sh

--- Gentoo1.sh
+++ Gentoo1.sh
@@ -2,8 +2,9 @@
 # Gentoo1
 # https://vk.com/@poruncov-chek-list-po-ustanovke-gemtoo-x86-64
 # https://wiki.gentoo.org/wiki/Handbook:AMD64/Full/Installation/ru#GUID_Partition_Table_.28GPT.29
-# https://wiki.gentoo.org/wiki/Dm-crypt_full_disk_encryption
-# https://wiki.gentoo.org/wiki/Safe_CFLAGS
+# https://wiki.gentoo.org/wiki/Dm-crypt_fu
+# https://www.youtube.com/watch?v=LdrhKCTCqs0
+# https://www.youtube.com/watch?v=3A9GH2WCR_g
 ping -c17 8.8.8.8
 
 # ls /dev/nvme*
